f41c8b3840ba17ba4210ad43297637111b3c6ebf
Ethan-Swift <ethan.swift@richmond.edu>
2023-11-02 12:42:05 -0400

    added main system lists and functions to header

--- include/hppfiles/system.hpp
+++ include/hppfiles/system.hpp
@@ -1,10 +1,21 @@
 #ifndef HPPFILES_SYSTEM_HPP
 #define HPPFILES_SYSTEM_HPP
+#include<list>
 
+using namespace std;
 namespace system
 {
     class system {
         private:
-        
+        std::list<robot> *robots;
+        std::list<room> *rooms;
+
+        public:
+        list<string> queryRobotStatus(list<robot> listRobots);
+        list<string> queryRoomStatus(list<room> listRooms);
+        void clean(room room,list<robot> listRobots);
+        void repair(string robot);
+        void recharge(string robot);
+
     }
 }
