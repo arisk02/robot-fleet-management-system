62e68380ed7fc80b8a5b3faad0ffc512204219b6
Ethan-Swift <ethan.swift@richmond.edu>
2023-11-06 21:07:02 -0500

    started system.cpp

--- include/hppfiles/system.hpp
+++ include/hppfiles/system.hpp
@@ -1,21 +1,24 @@
 #ifndef HPPFILES_SYSTEM_HPP
 #define HPPFILES_SYSTEM_HPP
-#include<list>
+#include <list>
+#include "robot.hpp"
+#include "room.hpp"
 
 using namespace std;
 namespace system
 {
     class system {
         private:
-        std::list<robot> *robots;
-        std::list<room> *rooms;
+        std::list<Robot::Robot> *robots;
+        std::list<Room::Room> *rooms;
 
         public:
-        list<string> queryRobotStatus(list<robot> listRobots);
-        list<string> queryRoomStatus(list<room> listRooms);
-        void clean(room room,list<robot> listRobots);
+        list<string> queryRobotStatus(list<Robot::Robot> listRobots);
+        list<string> queryRoomStatus(list<Room::Room> listRooms);
+        void clean(Room::Room room,list<Robot::Robot> listRobots);
         void repair(string robot);
         void recharge(string robot);
 
     }
 }
+#endif
---
+++ src/system.cpp
@@ -0,0 +1,14 @@
+#include "hppfiles/system.hpp"
+
+system::system() : {
+    int smallMopper = 1;
+    int largeMopper = 2;
+    int smallVaccume = 2;
+    int largeVaccume = 3;
+    int smallScrubber = 1;
+    int largeScrubber = 1;
+    for (int i = 0, i < smallMopper, i++) {
+        
+    }
+
+}
