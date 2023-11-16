8b46e32b7d8580b2750f24eeeba39bc22c922956
arisk02 <ariskaramustafic2002@gmail.com>
2023-11-15 21:32:54 -0500

    created constructor with parameters

--- include/hppfiles/cleaningSystem.hpp
+++ include/hppfiles/cleaningSystem.hpp
@@ -16,9 +16,15 @@ namespace cleaningSys
         std::vector<Robot*> robots;
         std::vector<Room> rooms;
         vector<Robot*> getRobots(vector<int> ids);
+        int roomCounter = 0;
+        int robotCounter = 0;
 
         public:
         cleaningSystem();
+        cleaningSystem(int smallScrubbers, int mediumScrubbers, int largeScrubbers,
+                       int smallVacuums, int mediumVacuums, int largeVacuums,
+                       int smallMoppers, int mediumMoppers, int largeMoppers,
+                       int smallRooms, int mediumRooms, int largeRooms);
         ~cleaningSystem();
         vector<string> queryRobotStatus(vector<int> listRobots);
         vector<string> queryRoomStatus(vector<int> listRooms);
