a1063797d1743d42bf7fe795d53ea8f7a590c1de
Berk-cs <bereket.zewdu@richmond.edu>
2023-11-28 14:11:56 -0500

    implemented repair and recharge

--- src/cleaningSystem.cpp
+++ src/cleaningSystem.cpp
@@ -173,10 +173,16 @@ namespace cleaningSys {
         }
     }
     void cleaningSystem::repair(vector<int> botIds){
-        
+        vector<Robot*> bots = getRobots(botIds);
+        for (auto robot: bots) {
+            robot.chargeRobot();
+        }                      
     }
     void cleaningSystem::recharge(vector<int> botIds){
-        
+        vector<Robot*> bots = getRobots(botIds);
+        for (auto robot: bots) {
+            robot.fixRobot();
+        }
     }
 
 }
